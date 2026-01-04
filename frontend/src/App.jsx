a299a618c7b9b281590fb78bb0b1241c79e78d76
Ruinius <tiger.huang2009@gmail.com>
2026-01-04 07:59:18 -0800

    Revamp UI styling for premium look

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import Dashboard from './pages/Dashboard'
@@ -47,7 +47,9 @@ function App() {
     <ThemeProvider>
       <AuthProvider>
         <Router>
-          <AppRoutes />
+          <div className="app">
+            <AppRoutes />
+          </div>
         </Router>
       </AuthProvider>
     </ThemeProvider>
@@ -55,5 +57,3 @@ function App() {
 }
 
 export default App
-
-
